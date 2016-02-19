bc6baf53483bce22978b4ee4e228642b43ffd0cb
Jan Moxter <jan.moxter@innobix.com>
2016-02-19 16:17:46 -0500

    - Issue #12: The fieldUpdate Javascript fails for big index numbers
    closes #12

--- src/main/efaps/org/efaps/ui/wicket/behaviors/FieldUpdate.js
+++ src/main/efaps/org/efaps/ui/wicket/behaviors/FieldUpdate.js
@@ -68,58 +68,60 @@ function eFapsSetFieldValue(_referenceIdOrIdx, _fieldName, _fieldValue, _fieldLa
             if (fields.length > 1) {
                 var cp = pos;
             }
-            if (_fieldValue instanceof Array) {
-                var sel = fields[cp];
-                if (sel.nodeName == 'SELECT') {
-                } else {
-                    sel = document.createElement('SELECT');
-                    for (i = 0; i < fields[cp].attributes.length; i++) {
-                        if (sel.getAttributeNode(fields[cp].attributes[i].name) == null) {
-                            sel.setAttribute(fields[cp].attributes[i].name, fields[cp].attributes[i].value);
-                        }
-                    }
-                    sel.size = 1;
-                    fields[cp].parentNode.replaceChild(sel, fields[cp]);
-                }
-                while (sel.options.length) {
-                    sel.options[0] = null;
-                }
-                for (i = 1; i < _fieldValue.length; i = i + 2) {
-                    option = new Option(_fieldValue[i + 1], _fieldValue[i], false, _fieldValue[i] == _fieldValue[0]);
-                    sel.options[sel.length] = option;
-                }
-            } else if (typeof(_fieldLabel) !== "undefined") { // that means that a dojo widget must be search
-                var parentWidget = registry.getEnclosingWidget(fields[cp]);
-                if (parentWidget != null && typeof(parentWidget) !== "undefined") {
-                    if (parentWidget.isInstanceOf(AutoComplete)) {
-                        // deactivate the onchange event
-                        parentWidget.set('_onChangeActive', false);
-                        parentWidget.set("item",{id: _fieldValue, name:_fieldLabel, label: _fieldLabel});
-                        // reactivate the onchange event
-                        parentWidget.set('_onChangeActive',true);
-                    } else if (parentWidget.isInstanceOf(AutoTokenInput)) {
-                        // deactivate the onchange event
-                        parentWidget.set('_onChangeActive', false);
-                        if (_fieldValue) {
-                            parentWidget.addToken(_fieldValue, _fieldLabel);
-                        } else {
-                            parentWidget.clear();
-                        }
-                        // reactivate the onchange event
-                        parentWidget.set('_onChangeActive', true);
-                   }
-                }
-            } else {
-                // if it is an input, the value can be set directly, else the DOM must be used
-                if (fields[cp].nodeName == 'INPUT' || fields[cp].nodeName == 'TEXTAREA'
-                        || fields[cp].nodeName == 'SELECT') {
-                    fields[cp].value = _fieldValue;
-                } else {
-                    if (fields[cp].hasChildNodes()) {
-                        fields[cp].firstChild.data = _fieldValue;
+            if (fields.length > cp) {
+                if (_fieldValue instanceof Array) {
+                    var sel = fields[cp];
+                    if (sel.nodeName == 'SELECT') {
                     } else {
-                        var n = document.createTextNode(_fieldValue);
-                        fields[cp].appendChild(n);
+                        sel = document.createElement('SELECT');
+                        for (i = 0; i < fields[cp].attributes.length; i++) {
+                            if (sel.getAttributeNode(fields[cp].attributes[i].name) == null) {
+                                sel.setAttribute(fields[cp].attributes[i].name, fields[cp].attributes[i].value);
+                            }
+                        }
+                        sel.size = 1;
+                        fields[cp].parentNode.replaceChild(sel, fields[cp]);
+                    }
+                    while (sel.options.length) {
+                        sel.options[0] = null;
+                    }
+                    for (i = 1; i < _fieldValue.length; i = i + 2) {
+                        option = new Option(_fieldValue[i + 1], _fieldValue[i], false, _fieldValue[i] == _fieldValue[0]);
+                        sel.options[sel.length] = option;
+                    }
+                } else if (typeof(_fieldLabel) !== "undefined") { // that means that a dojo widget must be search
+                    var parentWidget = registry.getEnclosingWidget(fields[cp]);
+                    if (parentWidget != null && typeof(parentWidget) !== "undefined") {
+                        if (parentWidget.isInstanceOf(AutoComplete)) {
+                            // deactivate the onchange event
+                            parentWidget.set('_onChangeActive', false);
+                            parentWidget.set("item",{id: _fieldValue, name:_fieldLabel, label: _fieldLabel});
+                            // reactivate the onchange event
+                            parentWidget.set('_onChangeActive',true);
+                        } else if (parentWidget.isInstanceOf(AutoTokenInput)) {
+                            // deactivate the onchange event
+                            parentWidget.set('_onChangeActive', false);
+                            if (_fieldValue) {
+                                parentWidget.addToken(_fieldValue, _fieldLabel);
+                            } else {
+                                parentWidget.clear();
+                            }
+                            // reactivate the onchange event
+                            parentWidget.set('_onChangeActive', true);
+                       }
+                    }
+                } else {
+                    // if it is an input, the value can be set directly, else the DOM must be used
+                    if (fields[cp].nodeName == 'INPUT' || fields[cp].nodeName == 'TEXTAREA'
+                            || fields[cp].nodeName == 'SELECT') {
+                        fields[cp].value = _fieldValue;
+                    } else {
+                        if (fields[cp].hasChildNodes()) {
+                            fields[cp].firstChild.data = _fieldValue;
+                        } else {
+                            var n = document.createTextNode(_fieldValue);
+                            fields[cp].appendChild(n);
+                        }
                     }
                 }
             }
